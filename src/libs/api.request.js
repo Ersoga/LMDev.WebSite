4147cce9737bbb534443ba7a4f14d8e490d15717
刘 民 <1443619775@qq.com>
2022-12-28 11:22:34 +0800

    remove libs store

--- src/libs/api.request.js
+++
@@ -1,6 +0,0 @@
-import HttpRequest from './axios'
-import config from '../config'
-const baseUrl = process.env.API_URL
-const defaultPrefix = config.ApI_VERSION;
-const axios = new HttpRequest(baseUrl,defaultPrefix)
-export default axios
