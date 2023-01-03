84f820bbb84f5e7966e095a20ff1c67f54cedf29
刘 民 <1443619775@qq.com>
2023-01-03 11:50:10 +0800

    dont newTab login

--- src/libs/util.js
+++ src/libs/util.js
@@ -1,9 +1,15 @@
 import * as React from 'react'
 export const TOKEN_KEY = ''
 export function setToken(token){
+    if (typeof window === 'undefined' || typeof window.localStorage === 'undefined') {
+        return false;
+    }
     window.localStorage.setItem(TOKEN_KEY, token);
 }
 export function getToken() {
+    if (typeof window === 'undefined' || typeof window.localStorage === 'undefined') {
+        return "";
+    }
     const token = window.localStorage.getItem(TOKEN_KEY);
     if (token) return token
     else return ""
