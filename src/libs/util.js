25a136dad5960a7dc454c57a39bc964815ae5380
刘 民 <1443619775@qq.com>
2023-01-03 11:20:49 +0800

    localStreage add window

--- src/libs/util.js
+++ src/libs/util.js
@@ -1,10 +1,10 @@
 
 export const TOKEN_KEY = ''
 export function setToken(token){
-    localStorage.setItem(TOKEN_KEY, token);
+    window.localStorage.setItem(TOKEN_KEY, token);
 }
 export function getToken() {
-    const token = localStorage.getItem(TOKEN_KEY);
+    const token = window.localStorage.getItem(TOKEN_KEY);
     if (token) return token
     else return ""
   }
